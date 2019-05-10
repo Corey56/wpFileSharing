ccf34fcac4183afb812e1785803623624b16c7f7
Cadet <cadet@cadet-vm.ninja>
2019-05-09 21:57:59 -0400

    Working on tests no flags

--- final_proj/mysite/wpFiles/tests.py
+++ final_proj/mysite/wpFiles/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from wpFiles.models import Academic_dept, Academic_class, Upload
 # Create your tests here.
+
+class Academic_deptTests(TestCase):
+    def test_is_dep(self):
+        dep_real = Academic_dept.objects.get(dept_code="EECS")
+        self.assertIs(dep_real, True)
+        dep_fake = Academic_dept(dept_code="DEL")
+        self.assertIs(Academic_dept.objects.get(dept_code="DPE"), False)

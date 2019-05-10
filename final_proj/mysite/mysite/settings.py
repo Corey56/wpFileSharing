fbfc360f27d10c70c4f9a1d1b57de0e1f0901a49
Cadet <cadet@cadet-vm.ninja>
2019-05-09 22:14:12 -0400

    S3 backend functioning

--- final_proj/mysite/mysite/settings.py
+++ final_proj/mysite/mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'star_ratings',
     'watson',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,22 @@ MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = '/wpFiles'
 LOGOUT_REDIRECT_URL = '/wpFiles'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'wpFiles/static'),
+]
+
+AWS_ACCESS_KEY_ID = 'AKIAZ7FKHHSIZFCCZ3OR'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'wpfiles-assets'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
